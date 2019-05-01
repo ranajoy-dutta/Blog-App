7dfe5056a612ac6b3f76ec5db93048d3720c535d
Ranajoy <ranajoydutta7@gmail.com>
2019-05-01 22:18:02 +0530

    Routing Improvised
    Removed index route for welcome controller and added all routes through Article controller.
    All articles method moved to separate route.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-
+	
   resources :articles do
   	resources :comments
   end
-    
-  root 'welcome#index'
+  get '/posts' => 'articles#home'
+  root 'articles#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
