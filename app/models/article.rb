90254e3c7c707f0232c9ffb78f3bcbbfb2463a13
Ranajoy <ranajoydutta7@gmail.com>
2019-05-03 00:10:55 +0530

    Add Devise
    Added devise gem

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,5 @@ class Article < ApplicationRecord
 	has_attached_file :image, styles: { large:"800x600>", medium: "400x300>", thumb: "120x120#" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
   
+  belongs_to :user
 end
